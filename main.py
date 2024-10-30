1aeedda00e0095af992b2050cef1680708af5be9
Niarea <51544335+Niarea@users.noreply.github.com>
2024-10-30 04:35:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -97,4 +97,4 @@ async def predict(file: UploadFile = File(...)):
 # Commande pour démarrer le serveur FastAPI
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000
