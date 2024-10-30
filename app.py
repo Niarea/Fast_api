0774ad3cd7b317b6864f28897e54efa9de3c66be
Niarea <51544335+Niarea@users.noreply.github.com>
2024-10-30 20:02:03 +0100

    Update and rename main.py to app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from fastapi import FastAPI, File, UploadFile
-from tensorflow.keras.models import load_model # type: ignore
-from PIL import Image, ImageOps
+from keras.models import load_model
+from PIL import Image
 from scipy.stats import entropy
 import numpy as np
 import io
 import time
-from tensorflow.keras.layers import DepthwiseConv2D  # type: ignore
-
 
 # Initialiser l'application FastAPI
 app = FastAPI(
@@ -21,40 +19,22 @@ app = FastAPI(
 def read_root():
     return {"message": "API Dental Diseases Prediction"}
 
-def custom_depthwise_conv2d(*args, **kwargs):
-    if 'groups' in kwargs:
-        del kwargs['groups']  # Retirer 'groups'
-    return DepthwiseConv2D(*args, **kwargs)
+# Charger le modèle ML (assurez-vous d'ajuster le chemin si nécessaire
+model = load_model('Model3.keras')
 
-# Charger le modèle
-model = load_model("Model3.keras", compile=False)
-
-# Charger les étiquettes
-with open("labels.txt", "r") as file:
-    class_names = [line.strip() for line in file.readlines()]
-
-# Fonction pour prétraiter l'image
-def preprocess(image):
-    # Redimensionner l'image à 224x224
-    image = ImageOps.fit(image, (180, 180), Image.Resampling.LANCZOS)
-    
-    # Convertir l'image en tableau numpy et s'assurer qu'elle a 3 canaux
-    image_array = np.asarray(image.convert("RGB"), dtype=np.float32)
-    
-    # Normaliser l'image
-    normalized_image_array = (image_array / 127.5) - 1
-    
-    # Créer le tableau de données pour le modèle
-    data = np.ndarray(shape=(1, 180, 180, 3), dtype=np.float32)
-    data[0] = normalized_image_array
-
-    return data
+# Fonction pour pré-traiter l'image avant de la passer au modèle
+def preprocess_image(image):
+    image = image.resize((1, 96))  # Ajustez la taille selon votre modèle
+    image = np.array(image) / 255.0   # Normalisation
+    image = np.expand_dims(image, axis=0)  # Ajouter une dimension batch
+    return image
 
 # Endpoint pour les prédictions
+
 @app.post("/predict/", summary="Upload an image for dental disease prediction", description="This endpoint allows you to upload an image of the teeth and get a prediction of the dental disease.")
 async def predict(file: UploadFile = File(...)):
     """
-    Charge une image et la passe au modèle pour prédire.
+    Charge une image de 96x96 pixels et la passe au modèle pour prédire.
     
     - **file**: fichier image (jpeg ou png) à analyser
     """
@@ -62,29 +42,32 @@ async def predict(file: UploadFile = File(...)):
     try:
         image = Image.open(io.BytesIO(await file.read()))
     except Exception as e:
-        return {"error": f"Invalid image file: {str(e)}"}
+        return {"error": "Invalid image file"}
     
     # Pré-traiter l'image
-    processed_image = preprocess(image)
-    
-    # Calculer le temps de traitement
-    start_time = time.time()
+    processed_image = preprocess_image(image)
     
     # Faire la prédiction avec le modèle
     prediction = model.predict(processed_image)
-    processing_time = time.time() - start_time
-
+    
+    # Liste des classes (ajuste les noms selon tes classes réelles)
+    classes = ["Calculus", "Dental Caries", "Gingivitis", "Hypodontia", "Toot Discoloration"]
+    
     # Trouver la classe avec la probabilité la plus élevée
     predicted_class_index = np.argmax(prediction[0])
-    predicted_class = class_names[predicted_class_index]
+    predicted_class = classes[predicted_class_index]
     
     # Calculer la marge de confiance
     sorted_probs = np.sort(prediction[0])[::-1]
     confidence_margin = sorted_probs[0] - sorted_probs[1]
     
+    # Calculer le temps de traitement
+    start_time = time.time()
+    prediction = model.predict(processed_image)
+    processing_time = time.time() - start_time
+
     # Calculer l'entropie
     uncertainty = entropy(prediction[0])  # calcul de l'entropie
-    
     # Retourner la prédiction sous forme JSON
     return {
         "prediction": predicted_class,
@@ -97,4 +80,4 @@ async def predict(file: UploadFile = File(...)):
 # Commande pour démarrer le serveur FastAPI
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
